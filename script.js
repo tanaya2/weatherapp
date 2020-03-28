8d223a181d513cd0116e91fad7466c15460e556e
tanaya2 <47844972+tanaya2@users.noreply.github.com>
2020-03-28 16:43:09 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,177 +1,182 @@
-script src="https://code.jquery.com/jquery-3.2.1.min.js"   integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="   crossorigin="anonymous"></script>
+$(document).ready(function () {
 
-        <script type="text/javascript">
-            $(document).ready(function(){
-                
-            var newLocation = '';
-                
-            if (navigator.geolocation) {
-            console.log(navigator.geolocation);
-                    
-                function success(pos) {
-                console.log(pos);
-                    
-                    var crds = pos.coords;
-                    console.log('my current position is: ');
-                    console.log('lat: ' + crds.latitude);
-                    console.log('long:' + crds.longitude);
-                    console.log('more or less:' + crds.accuracy + 'm away.');
-                        
-                //set string for data for dark sky api based on their format
-                        
-                newlocation = crds.latitude + ',' + crds.longitude;
-                        
-                        //this will set the string to retrive location based on open sky formate
-                        var latlongName = crds.latitude + '+' + crds.longitude;
-                        
-                        //run function to get weather data from api
-                        getWeatherData(newLocation);
-                        
-                        //function to get location name
-                        getLocationName(latlongName);
-                        
-                }
-                    
-                function error(err) {
-                console.log(err);
-                        
-                    //default location is canberra
-                    var defaultLocation = '-35.28346,1249.12807';
-                        
-                    getWeatherData(newLocation);
-                }
-           
-                //this is the line that triggers the browser prompt
-                navigator.geolocation.getCurrentPosition(success, error);
-                    
-                }
+    var newLocation = '';
 
-    });//close document ready
-            
-    //get location name
-            
-        function getLocationName(latlngCoords) {
+    if (navigator.geolocation) {
+        console.log(navigator.geolocation);
+
+        function success(pos) {
+            console.log(pos);
+
+            var crds = pos.coords;
+            //            console.log('my current position is: ');
+            //            console.log('lat: ' + crds.latitude);
+            //            console.log('long:' + crds.longitude);
+            //            console.log('more or less:' + crds.accuracy + 'm away.');
+
+            //set string for data for dark sky api based on their format
+
+            newLocation = crds.latitude + ',' + crds.longitude;
+
+            //this will set the string to retrive location based on open sky formate
+            var latlongName = crds.latitude + '+' + crds.longitude;
+
+            console.log(newLocation);
+
+            //run function to get weather data from api
+            getWeatherData(newLocation);
+
+            //function to get location name
+            getLocationName(latlongName);
+
+        }
+
+        function error(err) {
+            console.log(err);
+
+            //set default location - canberra
+            var defaultLocation = '-35.28346,149.12807';
+
+            getWeatherData(newLocation);
+        }
+
+        //this is the line that triggers the browser prompt
+        navigator.geolocation.getCurrentPosition(success, error);
+
+    }
+
+}); //close document ready
+
+//get location name
+
+function getLocationName(latlngCoords) {
+
+    var apiKey = 'c89f5a299176437ea002fecc8d8b544b';
+
+    var geocodeUrl = 'http://api.opencagedata.com/geocode/v1/json?q=' + latlngCoords + '&key=' + apiKey;
+
+    $.get(geocodeUrl, function (locationData) {
+        console.log(locationData.results[0]);
+
+        var locationComponent = locationData.results[0].components;
+
+        var locString = locationComponent.suburb + ', ' + locationComponent.state_code;
+
+        var location = $('<h1>').text("In: " + locString);
+
+        //add to body
+        $("currently").append(location);
+
+    });
+
+};
+
+//this function will load data from darksky api
+function getWeatherData(currentLocation) {
+
+    //my secret key
+    var key = '80fa44f25c6d60fb504e7a003ddd88ad';
+
+
+    //api call
+    var url = 'https://api.darksky.net/forecast/' + key + '/' + currentLocation + '?units=auto&callback=?';
+    console.log(url);
+
+    $.getJSON(url, function (data) {
+
+        //create new date object
+        var now = new Date(data.currently.time * 1000);
+        //get date and display
+        var time = $("<p>").text("At: " + now.toDateString() + ", " + now.toTimeString());
+        //add to body
+        //$("#currently").append(time);
         
-            var apiKey = 'c89f5a299176437ea002fecc8d8b544b'; 
+        //get summary
+       // var summary = $("<h2>").html("" + data.currently.summary);
+        //$("#currently").append(summary);
+        
+        var iconImage = ''; //set empty image string
+        var cIcon = data.currently.icon; //shortcut for data
+
+        //icon image files
+        var sunIcon = 'assets/images/sun.png';
+        var cloudSunIcon = 'assets/images/suncloud.png';
+        var cloudMoonIcon = 'assets/images/cloudnight.png';
+        var moonIcon = 'assets/images/night.png';
+        var rainIcon = 'assets/images/rain.png';
+        var windyIcon = 'assets/images/wind.png';
+        var cloudyIcon = 'assets/images/cloud.png';
+        var coldIcon = 'assets/images/cold.png';//display the right icon
+
+        //mega if statement 
+        // there are other more efficient ways to do this... but you can see the logic here. 
+        if (cIcon === 'clear-day') {
+            iconImage = sunIcon;				
+        } else if (cIcon === 'cloudy') {
+            iconImage = cloudyIcon;		
+        } else if (cIcon === 'rain') {
+            iconImage = rainIcon;
+        } else if (cIcon === 'clear-night') {
+            iconImage = moonIcon;
+        } else if (cIcon === 'partly-cloudy-night') {
+            iconImage = cloudMoonIcon;
+        } else if (cIcon === 'partly-cloudy-day') {
+            iconImage = cloudSunIcon;
+        } else if (cIcon === 'snow') {
+            iconImage = coldIcon;
+        } else if (cIcon === 'wind') {
+            iconImage = windyIcon;
+        }
+        $('.icons').html('<img src="' + iconImage + '">');
+           
+        //get current temp
+        var currently = $("<h1>").html (" " + Math.round (data.currently.temperature) + "&deg;C");
+        //add it to body
+        $("#currently").append(currently);
+
+        //get apparent temperature
+        var apparentTemperature = $("<p>").html("Feels like: " + Math.round (data.currently.apparentTemperature) + "&deg;C");
+        $("#currently").append(apparentTemperature);
+
+        //loop through the data and add it the table
+        //new row for each new item
+
+        for (var i = 0; i < data.daily.data.length; i++) {
+            var f = data.daily.data[i]; // the data for one day in the forecast
+
+            console.log(f);
+
+            var row = $("<tr>");
+            var date = new Date(f.time * 1000);
             
-            var geocodeUrl = 'http://api.opencagedata.com/geocode/v1/json?q=' + latlngCoords + '&key=' + apiKey; 
-                
-                $.get(geocodeUrl, function(locationData) {
-                console.log(locationData.results[0]);
-                    
-                    var locationComponent = locationData.results[0].components;
-                    
-                    var locString = locationComponent.suburb + ', ' + locationComponent.state_code;
-                    
-                    var location = $('<h1>').text("In: " + locString);
-                    
-                    //add to body
-                    $("currently").append(location);
-                    
-                });
-                
-            };
+            //icon summary
+            row.append("<td>" + f.summary + "</td>");
             
-        //this function will load data from darksky api
-        function getWeatherData(currentLocation) {
+            // date
+            row.append("<td>" + date.toDateString() + "</td>");
 
-                //my secret key
-                var key = '80fa44f25c6d60fb504e7a003ddd88ad';
-                
-                //set default location - canberra
-                var defaultLocation = '-35.28346,149.12807';
+            //high
+            row.append("<td class='temp'>" + Math.round(f.temperatureMax) + "&deg;C</td>");
 
-                //api call
-                var url = 'https://api.darksky.net/forecast/80fa44f25c6d60fb504e7a003ddd88ad/-35.28346,149.12807?units=auto'
-                $.get(url, function(data) {
+            //low
+            row.append("<td class='temp'>" + Math.round(f.temperatureMin) + "&deg;C</td>");
 
-                    
-                    //create new date object
-                    var now = new Date(data.currently.time*1000);
-                    //get date and display
-                    var time = $("<h1>").text("At: " + now.toDateString() + ", " + now.toTimeString() );
-                    //add to body
-                    $("#currently").append(time);
+            //summary
+            row.append("<td>" + f.summary + "</td>");
+            
 
-                    //get current temp
-                    var currently = $("<h2>").html("Currently: " + data.currently.temperature + "&deg;C");
-                    //add it to body
-                    $("#currently").append(currently);
-                    
-                    //get daily low
-                    var temperatureMin = $("<h2>").html("Low: " + data.daily.temperatureMin + "&deg;C");
-                    $("#daily").append(temperatureMin);
-                    
-                    //get daily high
-                    var temperatureHigh = $("<h2>").html("High: " + data.currently.temperatureHigh + "&deg;C");
-                    $("#currently").append(temperatureHigh);
-                    
-                    //get the summary from API
-                    var summary = $("<h2>").text("Summary: " + data.currently.summary);
-                    //add it to body
-                    $("#currently").append(summary);
-                    
-                    //get apparent temperature
-                    var apparentTemperature = $("<h2>").html("Apparent Temperature: " + data.currently.apparentTemperature + "&deg;C");
-                    $("#currently").append(apparentTemperature);
-                    
-                    //get wind speed
-                     var windSpeed = $("<h2>").text("Wind Speed: " + data.currently.windSpeed*3.6 + "km/h");
-                    $("#currently").append(windSpeed);
-                    
-                    //Math.round(data.currently.windSpeed * 3.6) + " km/h";
-                                    
-                    
-                    //get precipiation chance
-                    var precipProbability = $("<h2>").text("Chance of Rain: " + data.currently.precipProbability*100 + "%" );
-                    $("#currently").append(precipProbability);
+            //wind
+            row.append("<td>" + Math.round(f.windSpeed *3.6) + "km/h </td>")
 
-                    
-                   
-
-                    //loop through the data and add it the table
-                    //new row for each new item
-
-                    for (var i = 0; i < data.daily.data.length; i++) {
-                        var f = data.daily.data[i];// the data for one day in the forecast
-
-                        console.log(f);
-
-                        var row = $("<tr>");
-                        var date = new Date(f.time*1000);
-                        // date
-                        row.append("<td>" + date.toDateString() + "</td>");
-                        
-                        //current
-                        
-                        
-                        //high
-                        row.append("<td class='temp'>" + Math.round(f.temperatureMax) + "&deg;C</td>");
-                        
-                        //low
-                        row.append("<td class='temp'>" + Math.round(f.temperatureMin) + "&deg;C</td>");
-                        
-                        //summary
-                        row.append("<td>" + f.summary + "</td>");
-                        
-                        //feelslike
-                        row.append("<td>" + f.apparentTemperature + "</td>")
-                        
-                        //wind
-                        row.append("<td>" + f.windSpeed + "</td>")
-                        
-                        //rain
-
-                        
-
-                        //append the tr info to the table
-                        $("#forecast").append(row);
-
-                    }
+            //rain
+            row.append("<td class='temp'>" + Math.round(f.precipProbability *100) + "%</td>");
 
 
+            //append the tr info to the table
+            $("#forecast").append(row);
 
-                });
-            }
-</body>
-        </script>
+        } // close loop
+
+
+    }); //close getJSON
+}
